28038f990ef77579d52264738019cdf03922a1c4
Game Changer <aditl.git@gmail.com>
2014-07-14 12:38:15 -0500

    Door stuff
    The door script, art, and implementation

---
+++ ADayInTheLife/Assets/Scripts/WorldObjects/Door.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+	public string SceneToLoad;
+
+	void OnTriggerStay(Collider col)
+	{
+		if(col.gameObject.name == "Player")
+		{
+			if(Input.GetKeyDown(KeyCode.Space))
+				Application.LoadLevel(SceneToLoad);
+		}
+	}
+}
