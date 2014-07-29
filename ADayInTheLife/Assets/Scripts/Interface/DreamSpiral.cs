b2918c898e555126485a2fde4d28c0c4b5caf2a2
Game Changer <aditl.git@gmail.com>
2014-07-29 09:50:53 -0500

    Added new dream spiral lines

--- ADayInTheLife/Assets/Scripts/Interface/DreamSpiral.cs
+++ ADayInTheLife/Assets/Scripts/Interface/DreamSpiral.cs
@@ -18,12 +18,12 @@ public class DreamSpiral : MonoBehaviour
 
     void Awake()
     {
-        _textMatertials[0] = Resources.Load("Interface/MainMenu/Materials/DreamSpiral/DreamSpiralText1") as Material;
-        _textMatertials[1] = Resources.Load("Interface/MainMenu/Materials/DreamSpiral/DreamSpiralText2") as Material;
-        _textMatertials[2] = Resources.Load("Interface/MainMenu/Materials/DreamSpiral/DreamSpiralText3") as Material;
-        _textMatertials[3] = Resources.Load("Interface/MainMenu/Materials/DreamSpiral/DreamSpiralText4") as Material;
-        _textMatertials[4] = Resources.Load("Interface/MainMenu/Materials/DreamSpiral/DreamSpiralText5") as Material;
-        _textMatertials[5] = Resources.Load("Interface/MainMenu/Materials/DreamSpiral/DreamSpiralText6") as Material;
+		_textMatertials[0] = Resources.Load("Interface/DreamSpiral/Materials/DreamText1") as Material;
+		_textMatertials[1] = Resources.Load("Interface/DreamSpiral/Materials/DreamText2") as Material;
+		_textMatertials[2] = Resources.Load("Interface/DreamSpiral/Materials/DreamText3") as Material;
+		_textMatertials[3] = Resources.Load("Interface/DreamSpiral/Materials/DreamText4") as Material;
+		_textMatertials[4] = Resources.Load("Interface/DreamSpiral/Materials/DreamText5") as Material;
+		_textMatertials[5] = Resources.Load("Interface/DreamSpiral/Materials/DreamText6") as Material;
         _startCloudActive = Resources.Load("Interface/MainMenu/Materials/StartCloudActive") as Material;
         _startCloudInactive = Resources.Load("Interface/MainMenu/Materials/StartCloudInactive") as Material;
 
@@ -36,7 +36,7 @@ public class DreamSpiral : MonoBehaviour
     void Start()
     {
         _startCloud.renderer.material = _startCloudInactive;
-		_textCloud.renderer.material = _textMatertials[Random.Range(0, 5)];
+		_textCloud.renderer.material = _textMatertials[Random.Range(0, 6)];
 		_secondSFX = GameObject.FindGameObjectWithTag("SecondSFX").GetComponent<AudioSource>();
 		_mainBGM = GameObject.FindGameObjectWithTag("MainBGM").GetComponent<AudioSource>();
     }
