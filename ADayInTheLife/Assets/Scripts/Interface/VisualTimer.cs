ec088112d7738a6ef8029545f9d3b9fb4547849a
mwarrington <mason.arrington@gmail.com>
2014-09-29 16:09:51 -0500

    Updated Visual Timer
    It is now a 3D object making it more versatile.

--- ADayInTheLife/Assets/Scripts/Interface/VisualTimer.cs
+++ ADayInTheLife/Assets/Scripts/Interface/VisualTimer.cs
@@ -4,172 +4,146 @@ using PixelCrushers.DialogueSystem;
 
 public class VisualTimer : MonoBehaviour
 {
-	protected bool showingGameTimer
-	{
-		get
-		{
-			if(_showingGameTimer)
-			{
-				if(DialogueManager.IsConversationActive)
-					_showingGameTimer = false;
-				else if(isItemActive)
-					_showingGameTimer = false;
-				else
-				{
-					foreach (SpriteRenderer sr in TimerCloud.GetComponentsInChildren<SpriteRenderer>())
-					{
-						sr.enabled = true;
-					}
-				}
-			}
-			else
-			{
-				foreach (SpriteRenderer sr in TimerCloud.GetComponentsInChildren<SpriteRenderer>())
-				{
-					sr.enabled = false;
-				}
-			}
+    protected bool showingGameTimer
+    {
+        get
+        {
+            if (_showingGameTimer)
+            {
+                if (DialogueManager.IsConversationActive)
+                    _showingGameTimer = false;
+                else if (isItemActive)
+                    _showingGameTimer = false;
+                else
+                {
+                    foreach (Renderer r in this.GetComponentsInChildren<Renderer>())
+                    {
+                        r.enabled = true;
+                    }
+                }
+            }
+            else
+            {
+                foreach (Renderer r in this.GetComponentsInChildren<Renderer>())
+                {
+                    r.enabled = false;
+                    Debug.Log(r.name);
+                }
+            }
 
-			return _showingGameTimer;
-		}
+            return _showingGameTimer;
+        }
 
-		set
-		{
-			_showingGameTimer = value;
-		}
-	}
-	private bool _showingGameTimer = false;
+        set
+        {
+            _showingGameTimer = value;
+        }
+    }
+    private bool _showingGameTimer = false;
 
-	protected bool isItemActive
-	{
-		get
-		{
-			foreach (ItemInteract ii in GameObject.FindObjectsOfType<ItemInteract>())
-			{
-				if(ii.ItemActive)
-				{
-					_isItemActive = true;
-					return _isItemActive;
-				}
-				else
-					_isItemActive = false;
-			}
-			return _isItemActive;
-		}
-	}
-	private bool _isItemActive;
-	
-	private GameManager _myGameManager;
-	private PlayerScript _player;
-	private int _gameTimer;
+    protected bool isItemActive
+    {
+        get
+        {
+            foreach (ItemInteract ii in GameObject.FindObjectsOfType<ItemInteract>())
+            {
+                if (ii.ItemActive)
+                {
+                    _isItemActive = true;
+                    return _isItemActive;
+                }
+                else
+                    _isItemActive = false;
+            }
+            return _isItemActive;
+        }
+    }
+    private bool _isItemActive;
 
-	public GUIStyle Font;
-	public GameObject TimerCloud;
+    private GameManager _myGameManager;
+    private PlayerScript _player;
+    private TextMesh _myTextMesh;
+    private int _gameTimer;
 
-	void Start()
-	{
-		_myGameManager = GameObject.FindObjectOfType<GameManager>();
-		_player = GameObject.FindObjectOfType<PlayerScript>();
+    void Start()
+    {
+        _myGameManager = GameObject.FindObjectOfType<GameManager>();
+        _player = GameObject.FindObjectOfType<PlayerScript>();
+        _myTextMesh = this.GetComponentInChildren<TextMesh>();
 
-		//This handles turning off the the Timer Cloud renderers
-		if(_myGameManager.Timer % 30 > 1)
-		{
-			showingGameTimer = false;
-		}
-	}
+        //This handles turning off the the Timer Cloud renderers
+        if (_myGameManager.Timer % 30 > 1)
+        {
+            showingGameTimer = false;
+        }
+    }
 
-	void OnGUI()
-	{
-		if(showingGameTimer)
-		{
-			GameTimer();
-		}
-	}
+    void OnGUI()
+    {
+        if (showingGameTimer)
+        {
+            GameTimer();
+        }
+    }
 
-	void Update()
-	{
-		//This handles when to turn on the Timer Cloud renderers
-		if(Application.loadedLevelName != "Hallway")
-		{
-			if(_myGameManager.Timer < 61)
-			{
-				_myGameManager.GameTimerActive = true;
-				showingGameTimer = true;
-			}
-			else if(_myGameManager.Timer % 30 < 1)
-			{
-				if(!showingGameTimer)
-				{
-					_myGameManager.GameTimerActive = true;
-					showingGameTimer = true;
-				}
-			}
-			else if(Input.GetKey(KeyCode.LeftShift) && !DialogueManager.IsConversationActive)
-			{
-				showingGameTimer = true;
-			}
-			else if(_myGameManager.Timer % 30 < 25 && showingGameTimer)
-			{
-				showingGameTimer = false;
-			}
-		}
-		else
-		{
-			if(_myGameManager.Timer < 61)
-			{
-				showingGameTimer = true;
-			}
-			else if(_myGameManager.Timer % 30 < 1 && _myGameManager.HasBeenIntroduced == true)
-			{
-				if(!showingGameTimer)
-				{
-					_myGameManager.GameTimerActive = true;
-					showingGameTimer = true;
-				}
-			}
-			else if(Input.GetKey(KeyCode.LeftShift) && !DialogueManager.IsConversationActive)
-			{
-				showingGameTimer = true;
-			}
-			else if(_myGameManager.Timer % 30 < 25 && showingGameTimer)
-			{
-				showingGameTimer = false;
-			}
-		}
-	}
-	
-	private void GameTimer()
-	{
-		_gameTimer = (int)_myGameManager.Timer;
-		int displaySeconds = Mathf.CeilToInt(_gameTimer) % 60;
-		int displayMinutes = Mathf.CeilToInt(_gameTimer) / 60;
-		string text = string.Format ("{0:00}:{1:00}", displayMinutes, displaySeconds);
+    void Update()
+    {
+        //This handles when to turn on the Timer Cloud renderers
+        if (Application.loadedLevelName != "Hallway")
+        {
+            Debug.Log(_myGameManager.Timer);
+            if (_myGameManager.Timer < 61)
+            {
+                _myGameManager.GameTimerActive = true;
+                showingGameTimer = true;
+            }
+            else if (_myGameManager.Timer % 30 < 1)
+            {
+                if (!showingGameTimer)
+                {
+                    _myGameManager.GameTimerActive = true;
+                    showingGameTimer = true;
+                }
+            }
+            else if (Input.GetKey(KeyCode.LeftControl) && !DialogueManager.IsConversationActive)
+            {
+                showingGameTimer = true;
+            }
+            else if (_myGameManager.Timer % 30 < 25 && showingGameTimer)
+            {
+                showingGameTimer = false;
+            }
+        }
+        else
+        {
+            if (_myGameManager.Timer < 61)
+            {
+                showingGameTimer = true;
+            }
+            else if (_myGameManager.Timer % 30 < 1 && _myGameManager.HasBeenIntroduced == true)
+            {
+                if (!showingGameTimer)
+                {
+                    _myGameManager.GameTimerActive = true;
+                    showingGameTimer = true;
+                }
+            }
+            else if (Input.GetKey(KeyCode.LeftControl) && !DialogueManager.IsConversationActive)
+            {
+                showingGameTimer = true;
+            }
+            else if (_myGameManager.Timer % 30 < 25 && showingGameTimer)
+            {
+                showingGameTimer = false;
+            }
+        }
+    }
 
-
-
-		//if (Application.loadedLevelName == "Hallway")
-		//	GUI.Box(new Rect(Screen.width * 0.14f, Screen.height * 0.4f, Screen.width * 0.15f, Screen.height * 0.15f), text, Font);
-		//else 
-		if (Application.loadedLevelName == "Labrary")
-			GUI.Box(new Rect(Screen.width * 0.14f, Screen.height * 0.4f, Screen.width * 0.15f, Screen.height * 0.15f), text, Font);
-		else if (Application.loadedLevelName == "Classroom" || Application.loadedLevelName == "Roomclass")
-			GUI.Box(new Rect(Screen.width * 0.265f, Screen.height * 0.367f, Screen.width * 0.15f, Screen.height * 0.15f), text, Font);
-		else if (Application.loadedLevelName == "Cafeteria")
-		{
-			Font.fontSize = 12;
-			GUI.Box(new Rect(Screen.width * 0.8f, ((Screen.height * 0.2f) * -_player.TranslateChange) + (Screen.height * 0.52f), Screen.width * 0.15f, Screen.height * 0.15f), text, Font);
-		}
-		else if(Application.loadedLevelName == "ConsularOffice")
-		{
-			Font.fontSize = 12;
-			GUI.Box(new Rect(Screen.width * 0.13f, Screen.height * 0.15f, Screen.width * 0.15f, Screen.height * 0.15f), text, Font);
-		}
-		else if(Application.loadedLevelName == "ChorusRoom")
-		{
-			Font.fontSize = 11;
-			GUI.Box(new Rect(Screen.width * 0.37f, Screen.height * 0.25f, Screen.width * 0.15f, Screen.height * 0.15f), text, Font);
-		}
-		else
-			GUI.Box(new Rect(Screen.width * 0.14f, Screen.height * 0.4f, Screen.width * 0.15f, Screen.height * 0.15f), text, Font);
-	}
+    private void GameTimer()
+    {
+        _gameTimer = (int)_myGameManager.Timer;
+        int displaySeconds = Mathf.CeilToInt(_gameTimer) % 60;
+        int displayMinutes = Mathf.CeilToInt(_gameTimer) / 60;
+        _myTextMesh.text = string.Format("{0:00}:{1:00}", displayMinutes, displaySeconds);
+    }
 }
