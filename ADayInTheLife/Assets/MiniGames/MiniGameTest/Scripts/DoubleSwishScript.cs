a24914b4f84feef8dcab5e8b454d42c55d82fe23
mwarrington <mason.arrington@gmail.com>
2014-10-09 16:22:32 -0500

    Start of mini game

---
+++ ADayInTheLife/Assets/MiniGames/MiniGameTest/Scripts/DoubleSwishScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoubleSwishScript : MonoBehaviour
+{
+    private int _swishCount = 0;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            animation.Play();
+            Invoke("CountIt", 0.75f);
+            Invoke("CountIt", 1f);
+        }
+    }
+
+    private void CountIt()
+    {
+        _swishCount++;
+    }
+}
